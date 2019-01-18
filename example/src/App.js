65de63cec44d9dbfd4295c18cb624a18cf6baf44
Max Beier <beierm@htw-berlin.de>
2019-01-18 15:22:14 +0100

    added example

---
+++ example/src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { EdfHeaderVisualization } from 'edf-header-visualization';
+import edfHeader from './edf-header';
+
+const App = () => <EdfHeaderVisualization edfHeader={edfHeader} />;
+
+export default App;
