c789347b63b1927a8a3bd31b9f92a0fd3090e54d
Max Beier <beierm@htw-berlin.de>
2019-01-23 13:39:29 +0100

    updated example to accept user provided files

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
-import { EdfHeaderVisualization } from 'edf-header-visualization';
-import edfHeader from './edf-header';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import { EdfHeaderGrid, EdfHeaderLegend } from 'edf-header-visualization';
+import FileResource from './FileResource';
+import defaultHeader from './edf-header';
 
-const App = () => <EdfHeaderVisualization edfHeader={edfHeader} />;
+export default function EdfHeaderVisualization() {
+  const [hoveredItem, setHoveredItem] = useState('NONE');
+  const [edfHeader, seEdftHeader] = useState(defaultHeader);
+  const props = { edfHeader, hoveredItem, setHoveredItem };
 
-export default App;
+  const onDrop = async (files = []) => {
+    const resource = new FileResource(files[0]);
+    const staticHeader = await resource.read({ from: 0, till: 256 });
+    const numberOfSignals = +staticHeader.substr(252, 4);
+    const dynamicHeader = await resource.read({ from: 256, till: 256 + numberOfSignals * 256 });
+    seEdftHeader(staticHeader + dynamicHeader);
+  };
+
+  return (
+    <>
+      <Dropzone onDrop={onDrop}>
+        {({ getRootProps, getInputProps, isDragActive }) => (
+          <div className={`dropzone ${isDragActive ? 'active' : ''}`} {...getRootProps()}>
+            <input {...getInputProps()} />
+            This tools aims to help you understand EDFs static and dynamic header.<br />
+            You can inspect own files by <strong>clicking this area</strong> or <strong>dropping them onto it</strong>.
+          </div>
+        )}
+      </Dropzone>
+      <div className="edf-header-visualization flex">
+        <EdfHeaderGrid {...props} />
+        <EdfHeaderLegend {...props} />
+      </div>
+    </>
+  );
+}
